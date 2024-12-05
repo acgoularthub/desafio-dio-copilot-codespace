9c3563d96945037e43babf2b318c99dbc1a57796
Anderson Coelho Goulart <acgoulartmail@gmail.com>
2024-12-05 14:55:19 +0000

    finalizando o projeto

---
+++ concatenacao.py
@@ -0,0 +1,6 @@
+# gerar um código em python que concatene as entradas
+
+nome = input("Digite seu nome: ")
+sobrenome = input("Digite seu sobrenome: ")
+
+print("Seu nome completo é: ", nome + " " + sobrenome)
